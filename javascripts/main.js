f208dcae8d78eac4f8e4a657c29d303f684cbb7b
rossedfort <rossedfort@gmail.com>
2016-09-05 11:56:46 -0600

    style updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,34 +1,3 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage({
-    anchors: ['home', 'about'],
-    navigation: false,
-    css3: true,
-    scrollingSpeed: 1000,
-    scrollBar: false,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopBottom: false,
-    loopTop: false,
-    loopHorizontal: true,
-    continuousVertical: false,
-    normalScrollElements: '#element1, .element2',
-    scrollOverflow: false,
-    touchSensitivity: 15,
-    normalScrollElementTouchThreshold: 5,
-    slidesNavigation: true,
-    slidesNavPosition: 'bottom',
-    //Accessibility
-    keyboardScrolling: true,
-    animateAnchor: true,
-    recordHistory: true,
-
-    //Design
-    controlArrows: false,
-    verticalCentered: true,
-    resize : false,
-    sectionsColor: ['#FFFFFF', '#FFFFFF'],
-    fixedElements: '#header, #footer',
-    responsiveWidth: 600,
-    responsiveHeight: 0,
-  });
+  console.log('hello');
 });
